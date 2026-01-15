e220dee11bf97aa8e25c2288d48046b471b60cff
Nagato <transcendentalliouville@gmail.com>
2026-01-15 21:13:30 +0900

    ポテンシャルの記事を追加＆加筆

--- macros.js
+++ macros.js
@@ -3,7 +3,9 @@ window.MathJax = {
         macros: {
             bm: ["{\\boldsymbol{#1}}", 1],
             norm: ["{\\left\\|{#1}\\right\\|}", 1],
-            Pot: ["{\\mathrm{Pot}(#1)}", 1]
+            Pot: ["{\\mathrm{Pot}(#1)}", 1],
+            vol: ["{\\mathrm{vol}(#1)}", 1],
+            slope: ["{\\mathrm{sl}(#1)}", 1]
         }
     }
 };
