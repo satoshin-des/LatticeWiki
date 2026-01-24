e0baadbd79873208a3fa6ffdad2d0d078508ef42
Nagato <transcendentalliouville@gmail.com>
2026-01-24 14:51:06 +0900

    Hermiteの定数に関する記事を追加し，それに伴って目次を変更

--- macros.js
+++ macros.js
@@ -7,11 +7,13 @@ window.MathJax = {
             vol: ["{\\mathrm{vol}(#1)}", 1],
             slope: ["{\\mathrm{sl}(#1)}", 1],
             Z: ["{\\mathbb{Z}}"],
+            N: ["{\\mathbb{N}}"],
             R: ["{\\mathbb{R}}"],
             inner: ["{\\left\\langle{#1}\\right\\rangle}", 1],
             det: ["{\\mathrm{det}\\left(#1\\right)}", 1],
             abs: ["{\\left|{#1}\\right|}", 1],
-            od: ["{\\mathrm{od}\\left(#1\\right)}", 1]
+            od: ["{\\mathrm{od}\\left(#1\\right)}", 1],
+            span: ["{\\mathrm{span}_{\\R}\\left(#1\\right)}", 1]
         }
     }
 };
