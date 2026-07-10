7f27d1e1be799b69d775b04bec89411ef9b5c6bb
Nagato <transcendentalliouville@gmail.com>
2026-07-10 20:38:59 +0900

    Modified from a-tag to NuxtLink-tag

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -14,14 +14,14 @@ import SubSection from '~/components/SubSection.vue';
     <SubSection>記事一覧（五十音順）</SubSection>
 
     <ul>
-        <li><a href="/gsa">幾何級数仮定</a></li>
+        <li><NuxtLink to="/gsa">幾何級数仮定</NuxtLink></li>
         <li><a href="volume.html">体積</a></li>
         <li><a href="orthogonality-defect.html">直交性欠陥</a></li>
         <li><a href="potential.html">ポテンシャル</a></li>
-        <li><a href="/deep-insertion">deep-insertion</a></li>
+        <li><NuxtLink to="/gsa">deep-insertion</NuxtLink></li>
         <li><a href="deeplll.html">DeepLLL基底簡約</a></li>
         <li><a href="hermite-const.html">Hermiteの定数</a></li>
-        <li><a href="/lll">LLL基底簡約</a></li>
+        <li><NuxtLink to="/lll">LLL基底簡約</NuxtLink></li>
         <li><a href="potlll.html">PotLLL基底簡約</a></li>
     </ul>
 </template>
